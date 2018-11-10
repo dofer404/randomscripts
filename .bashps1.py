4e8d2e8646d828737f0292a5a28187b7bfc0c01f
Fernando Pirani <feremp.i+dev@gmail.com>
2018-11-10 18:44:00 -0300

    git-add-submodule-dangerous

--- .bashps1.py
+++ .bashps1.py
@@ -9,23 +9,23 @@ username = os.environ['USER']
 pwd = os.getcwd()
 homedir = os.path.expanduser('~')
 if pwd.startswith(homedir):
-	pwd = pwd.replace(homedir, '~', 1)
+    pwd = pwd.replace(homedir, '~', 1)
 
 try:
     length = int(sys.argv[1])
     length = length-3
     start = length/2
     if start > 5:
-    	start = 7
-    end    = (length-start) * -1
+        start = 7
+    end = (length-start) * -1
 except:
     print >>sys.stderr, "Usage: $0 <length>"
     sys.exit(1)
 
 
-if length<1 or not (len(pwd) > (length+3)):
-	pass
+if length < 1 or not (len(pwd) > (length+3)):
+    pass
 else:
-	pwd = pwd[:start]+'...'+pwd[end:];
-	
+    pwd = pwd[:start]+'...'+pwd[end:]
+
 print ' %s> ' % (pwd)
